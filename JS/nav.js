40b724618357e34e4cf979e6d71cf807427a4e3f
aleksi.virolainen <aleksi.c.virolainen@gmail.com>
2025-10-02 13:38:30 +0300

    Update nav.js

--- JS/nav.js
+++ JS/nav.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("#main-nav a");
+  const currentPath = window.location.pathname.split("/").pop(); // current file name
 
+  navLinks.forEach(link => {
+    if (link.getAttribute("href") === currentPath) {
+      link.classList.add("active");
+    }
+  });
+});
